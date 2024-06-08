d80e7d776b228f9e2d4a227f6553095214dbf7d9
Saadat Khalid Awan <126876532+Saadat-Khalid@users.noreply.github.com>
2024-06-08 16:47:47 +0500

    Update app.py

--- laptop_price_prediction_app/app.py
+++ laptop_price_prediction_app/app.py
@@ -1,11 +1,3 @@
-import subprocess
-
-def install_packages():
-    subprocess.check_call(["pip", "install", "-r", "requirements.txt"])
-
-if __name__ == '__main__':
-    install_packages()
-    
 import streamlit as st
 import joblib
 import pandas as pd
